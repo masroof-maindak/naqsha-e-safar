9f4636c5cf55bc0013354b6f310205e7c62d192f
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-11-27 11:35:51 +0500

    patch: refactor out constants to separate file

---
+++ src/constants.py
@@ -0,0 +1,34 @@
+import os
+
+LHR_NAME_STR = "Lahore District, Pakistan"
+LHR_LAT = 31.456845
+LHR_LON = 74.407836
+
+CITYLINES_SRCS = [
+    (
+        "https://www.citylines.co/api/lahore/raw_source/sections",
+        "lahore_sections.geojson",
+    ),
+    (
+        "https://www.citylines.co/api/lahore/raw_source/stations",
+        "lahore_stations.geojson",
+    ),
+    (
+        "https://www.citylines.co/api/data/lahore/lines_systems_and_modes",
+        "lahore_lines_systems_and_modes.json",
+    ),
+]
+
+
+WORLDPOP_URL: str = "https://data.worldpop.org/GIS/Population_Density/Global_2000_2020_1km_UNadj/2020/PAK/pak_pd_2020_1km_UNadj_ASCII_XYZ.zip"
+WORLDPOP_DATA_DIR = "../data/worldpop"
+
+OSMNX_DATA_DIR = "../data/osmnx"
+
+SPEEDO_DATA_DIR = "../data/speedo"
+SPEEDO_GEOJSON_FPATH = os.path.join(SPEEDO_DATA_DIR, "speedo_sections.geojson")
+
+CITYLINES_DATA_DIR = "../data/citylines"
+METRO_ROUTES_FPATH = os.path.join(CITYLINES_DATA_DIR, "lahore_sections.geojson")
+
+AGGR_GRAPH_PATH = "../data/aggregated.graphml"
